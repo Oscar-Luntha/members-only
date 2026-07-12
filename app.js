a3633387d61b629975ecf8abf35c920bbc042b70
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-12 22:01:00 +0200

    initialize the session and passport instanses

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 require("dotenv").config();
 const express = require("express")
-const app = express()
 const path = require("node:path")
 const indexRouter = require("./routes/indexRouter")
+const pool = require("./db/pool")
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const authRouter = require("./routes/authRouter")
+
+const app = express()
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
+
+app.use(session({ 
+  secret: process.env.SESSION_SECRET, 
+  resave: false, 
+  saveUninitialized: false 
+}));
+
 app.use(express.urlencoded({extended: true}))
 app.use("/", indexRouter)
+app.use("/auth", authRouter)
 const PORT = process.env.PORT;
 app.listen(PORT, (error)=> {
     if(error){
