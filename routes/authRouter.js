17ad56bbc8e58f115e88dd99fc1759f3c0363d5e
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-12 22:32:24 +0200

    add post method for sign up

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,5 +4,5 @@ const authController = require("../controllers/authController")
 
 authRouter.get("/sign-up", authController.sign_upGet)
 authRouter.get("/sign-in", authController.sign_inGet)
-
+authRouter.post("/sign-up", authController.sign_upPost)
 module.exports = authRouter
