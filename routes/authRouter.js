cdf7cef34d141d901242c21d61279660ac2663c7
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-13 20:38:33 +0200

    use passport authenticate method to check user log in

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,14 @@
 const {Router} =  require("express")
 const authRouter = Router()
 const authController = require("../controllers/authController")
+const passport = require("passport")
 
+authRouter.post("/sign-in", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true, // This puts the error message into req.session.messages
+  })
+)
 authRouter.get("/sign-up", authController.sign_upGet)
 authRouter.get("/sign-in", authController.sign_inGet)
 authRouter.post("/sign-up", authController.validateSignUp ,authController.sign_upPost)
