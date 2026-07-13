1739725af416728502a240dadd0763ea30b43fdf
Oscar Luntha <lunthaoscar@gmail.com>
2026-07-13 21:44:56 +0200

    get clubhouse posts at the index controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,26 @@
-function homepageGet(req, res){
-    res.render("index")
+const pool = require("../db/pool");
+
+async function homepageGet(req, res, next) {
+    try {
+        const queryText = `
+            SELECT messages.id, messages.title, messages.text, messages.created_at, clubhouses.name AS clubhouse_name
+            FROM messages
+            JOIN clubhouses ON messages.clubhouse_id = clubhouses.id
+            ORDER BY RANDOM()
+            LIMIT 20;
+        `;
+        
+        const { rows: messages } = await pool.query(queryText);
+
+        res.render("index", { 
+            user: req.user, 
+            messages: messages 
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
     homepageGet
-}
+};
